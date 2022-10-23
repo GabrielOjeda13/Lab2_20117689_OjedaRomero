4cf53ebfb92ad7f1c1f1daba2446dfe2404aff51
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-10-23 03:44:45 -0300

    TDA image
    Se creo el constructor TDA image junto a los 3 selectores principales, para obtener el ancho, alto y la lista de pixeles de una imagen.

--- main.pl
+++ main.pl
@@ -1,12 +1,18 @@
-%Este es le primer Hola mundo.
-%Base de conocimiento.
-
-
+% Gabriel Ojeda
+% 20.117.689-1
+% Laboratorio 2 Prolog
 
 %VARIABLES
 
-%PREDICADOS
+%   Width: Corresponde al ancho de la imágen
+%   Height: Corresponde al alto de la imágen
+%   ListaPixels: Correspendo a una lista de elementos de cualquier tipo de pixel.
 
-%CONSTRUCTOR
+%Predicados.
+%CONSTRUCTOR TDA image.
+image(Width, Height, ListaPixels,[Width, Height, ListaPixels]).
 
-%SELECTORES
+%SELECTORES TDA image
+getWidth([Width|_],Width).
+getHeigth([_|[Height|_]],Height).
+getListaPixels([_|[_|[ListaPixels|_]]],ListaPixels).
