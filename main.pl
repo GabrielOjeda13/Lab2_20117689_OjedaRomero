a5e26b1d3859d71c8e9408647178b19139e58bf0
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-11-02 13:20:26 -0300

    FlipH Para todo tipo de imagen
    Se finiquito la funcion FlipH con el llamado a cada predicado correspondiente a cada pixel, esto dependiendo de la imagen. ademas se añadieron algunos comentarios, pero falta aun documentar y estructurar.

--- main.pl
+++ main.pl
@@ -97,56 +97,62 @@ validePixelIsPixmap([X|Y]):- isPixmap(X), validePixelIsPixmap(Y).
 imageIsPixmap(Img):-
     getListaPixels(Img,ListaPixeles), validePixelIsPixmap(ListaPixeles).
 
-%Predicado que da vuelta una imagen Horizontalmente
+%PREDICADO QUE DA VUELTA UNA IMAGEN HORIZONTALMENTE.
 
-insertarAlPrincipio( Elemento, [], [Elemento] ).
-insertarAlPrincipio( Elemento, Lista, [Elemento|Lista] ).
-
-pixelRotateH(Pixel, Ancho, NewPixel):-
+%----- PIXBIT -----
+pixelRotateHBit(Pixel, Ancho, NewPixel):-
     getX(Pixel, X),
     getY(Pixel, Y),
     getBit(Pixel, Bit),
     getDepthBit(Pixel, D),
-    (X < Ancho, NewX is X + 1; NewX is X - 1),
+    (X < Ancho-1, NewX is X + 1; NewX is X - 1),
     pixbit(NewX, Y, Bit, D, NewPixel).
 
-pixelsFlipH([], _, _):- !.
-pixelsFlipH([Pixel|Cola], Ancho, NewListaPixels) :-
-    pixelRotateH(Pixel, Ancho, NewPixel),
-    insertarAlPrincipio(NewPixel, NewListaPixels, ListaPixels),
-    pixelsFlipH(Cola, Ancho, ListaPixels).
+%HECHO
+pixelsFlipHBit([], _, []):- !.
+pixelsFlipHBit([Pixel|Cola], Ancho, [NewPixel|Cola2]) :-
+    pixelRotateHBit(Pixel, Ancho, NewPixel),
+    pixelsFlipHBit(Cola,Ancho,Cola2).
+
+
+%----- PIXHEX -----
+pixelRotateHHex(Pixel, Ancho, NewPixel):-
+    getX(Pixel, X),
+    getY(Pixel, Y),
+    getHex(Pixel, Hex),
+    getDepthHex(Pixel, D),
+    (X < Ancho-1, NewX is X + 1; NewX is X - 1),
+    pixhex(NewX, Y, Hex, D, NewPixel).
+
+%HECHO
+pixelsFlipHHex([], _, []):- !.
+pixelsFlipHHex([Pixel|Cola], Ancho, [NewPixel|Cola2]) :-
+    pixelRotateHHex(Pixel, Ancho, NewPixel),
+    pixelsFlipHHex(Cola,Ancho,Cola2).
+
+%----- PIXRGB -----
+pixelRotateHRgb(Pixel, Ancho, NewPixel):-
+    getX(Pixel, X),
+    getY(Pixel, Y),
+    getR(Pixel, R),
+    getG(Pixel, G),
+    getB(Pixel, B),
+    getDepthRgb(Pixel, D),
+    (X < Ancho-1, NewX is X + 1; NewX is X - 1),
+    pixrgb(NewX, Y, R, G, B, D, NewPixel).
+
+%HECHO
+pixelsFlipHRgb([], _, []):- !.
+pixelsFlipHRgb([Pixel|Cola], Ancho, [NewPixel|Cola2]) :-
+    pixelRotateHRgb(Pixel, Ancho, NewPixel),
+    pixelsFlipHRgb(Cola,Ancho,Cola2).
 
 imageFlipH(Img, NewImg) :-
     getListaPixels(Img, ListaPixeles),
     getWidth(Img, Ancho),
     getHeigth(Img, Alto),
-    pixelsFlipH(ListaPixeles, Ancho, NewListPixels),
-    image(Ancho, Alto, NewListPixels, NewImg).
-
-
-/*
-insertarAlPrincipio( Elemento, [], [Elemento] ).
-insertarAlPrincipio( Elemento, Lista, [Elemento|Lista] ).
-
-movePixelH(Ancho, Pixel, PixelOut) :-
-    pixbitd(X, Y, Bit, Depth, Pixel),
-    (  Y < Ancho
-    -> NewY is Y + 1     
-    ;  NewY is 0
-    ),
-    pixbitd(X, NewY, Bit, Depth, PixelOut).
-
-movePixelsHorizontally(Ancho, [Pixel|Resto], PixelsAcc, PixelsOut) :-
-    movePixelH(Ancho, Pixel, PixelOut),
-    insertarAlPrincipio(PixelOut, PixelsAcc, PixelsOut),
-    
-    
-moveH(ImageIn, ImageOut) :-
-    image(Largo, Ancho, PixelsIn, ImageIn),
-    movePixelsHorizontally(Ancho, PixelsIn, PixelsOut),
-	image(Largo, Ancho, PixelsOut, ImageOut).
-
-*/
-
+    ((imagenIsBimap(Img), pixelsFlipHBit(ListaPixeles, Ancho, NewListPixels), image(Ancho, Alto, NewListPixels, NewImg),!);
+    (imagenIsHexmap(Img), pixelsFlipHHex(ListaPixeles, Ancho, NewListPixels), image(Ancho, Alto, NewListPixels, NewImg),!);
+    (imageIsPixmap(Img), pixelsFlipHRgb(ListaPixeles, Ancho, NewListPixels), image(Ancho, Alto, NewListPixels, NewImg))).
 
 
