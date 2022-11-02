39eeccf2d1132dbba172a69136334ac0588d7fcb
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-11-02 01:03:58 -0300

    Predicado FlipH
    Se crearon los predicados para invertir una imagen horizontalmente en primera instancia para las imagenes de tipo pixbit

--- main.pl
+++ main.pl
@@ -2,6 +2,11 @@
 % 20.117.689-1
 % Laboratorio 2 Prolog
 
+%%FUNCIONES AUX
+appendElement( Elemento, [], [Elemento]).
+appendElement( Elemento, Lista [Elemento|Lista]).
+
+
 %%--------------- VARIABLES GENERALES ---------------.
 
 % VARIABLES IMAGE
@@ -92,14 +97,56 @@ validePixelIsPixmap([X|Y]):- isPixmap(X), validePixelIsPixmap(Y).
 imageIsPixmap(Img):-
     getListaPixels(Img,ListaPixeles), validePixelIsPixmap(ListaPixeles).
 
+%Predicado que da vuelta una imagen Horizontalmente
+
+insertarAlPrincipio( Elemento, [], [Elemento] ).
+insertarAlPrincipio( Elemento, Lista, [Elemento|Lista] ).
+
+pixelRotateH(Pixel, Ancho, NewPixel):-
+    getX(Pixel, X),
+    getY(Pixel, Y),
+    getBit(Pixel, Bit),
+    getDepthBit(Pixel, D),
+    (X < Ancho, NewX is X + 1; NewX is X - 1),
+    pixbit(NewX, Y, Bit, D, NewPixel).
+
+pixelsFlipH([], _, _):- !.
+pixelsFlipH([Pixel|Cola], Ancho, NewListaPixels) :-
+    pixelRotateH(Pixel, Ancho, NewPixel),
+    insertarAlPrincipio(NewPixel, NewListaPixels, ListaPixels),
+    pixelsFlipH(Cola, Ancho, ListaPixels).
+
+imageFlipH(Img, NewImg) :-
+    getListaPixels(Img, ListaPixeles),
+    getWidth(Img, Ancho),
+    getHeigth(Img, Alto),
+    pixelsFlipH(ListaPixeles, Ancho, NewListPixels),
+    image(Ancho, Alto, NewListPixels, NewImg).
 
 
+/*
+insertarAlPrincipio( Elemento, [], [Elemento] ).
+insertarAlPrincipio( Elemento, Lista, [Elemento|Lista] ).
 
+movePixelH(Ancho, Pixel, PixelOut) :-
+    pixbitd(X, Y, Bit, Depth, Pixel),
+    (  Y < Ancho
+    -> NewY is Y + 1     
+    ;  NewY is 0
+    ),
+    pixbitd(X, NewY, Bit, Depth, PixelOut).
 
+movePixelsHorizontally(Ancho, [Pixel|Resto], PixelsAcc, PixelsOut) :-
+    movePixelH(Ancho, Pixel, PixelOut),
+    insertarAlPrincipio(PixelOut, PixelsAcc, PixelsOut),
+    
+    
+moveH(ImageIn, ImageOut) :-
+    image(Largo, Ancho, PixelsIn, ImageIn),
+    movePixelsHorizontally(Ancho, PixelsIn, PixelsOut),
+	image(Largo, Ancho, PixelsOut, ImageOut).
 
-
-
-
+*/
 
 
 
