899f06e97506a101e630f1516ee619fa44d28821
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2022-12-17 18:08:43 -0300

    Ejemplos para creacion de Imagenes
    Se crearon ejemplos nuevos para crear imagenes de distintos tipos, ya sea pixbit, pixhex y pixrgb, cada una de estas con 3 ejemplos de prueba.

--- prueba_20117689_OjedaRomero.pl
+++ prueba_20117689_OjedaRomero.pl
@@ -5,25 +5,19 @@
 /*
 
 Crear una imagen tipo PixBit.
-pixbit( 0, 0, 1, 10, PA), 
-pixbit( 0, 1, 0, 20, PB), 
-pixbit( 1, 0, 0, 30, PC), 
-pixbit( 1, 1, 1, 4, PD), 
-image( 2, 2, [PA, PB, PC, PD], I),
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I).
+pixbit( 0, 0, 0, 75, PA), pixbit( 0, 1, 0, 65, PB), pixbit( 1, 0, 0, 55, PC), pixbit( 1, 1, 0, 45, PD), image( 2, 2, [PA, PB, PC, PD], I).
+pixbit( 0, 0, 1, 1, PA), pixbit( 0, 1, 1, 2, PB), pixbit( 1, 0, 1, 3, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I).
 
 Crear una imagen tipo Pixmap o rgb.
-pixrgb( 0, 0, 10, 10, 10, 10, P1), 
-pixrgb( 0, 1, 20, 20, 20, 20, P2), 
-pixrgb( 1, 0, 30, 30, 30, 30, P3), 
-pixrgb( 1, 1, 40, 40, 40, 40, P4), 
-image( 2, 2,[ P1, P2, P3, P4], I1).
+pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2,[ P1, P2, P3, P4], I1).
+pixrgb( 0, 0, 199, 199, 0, 10, P1), pixrgb( 0, 1, 199, 199, 0, 20, P2), pixrgb( 1, 0, 0, 199, 199, 30, P3), pixrgb( 1, 1, 0, 0, 199, 40, P4), image( 2, 2,[ P1, P2, P3, P4], I1).
+pixrgb( 0, 0, 255, 255, 255, 50, P1), pixrgb( 0, 1, 200, 200, 200, 40, P2), pixrgb( 1, 0, 150, 150, 150, 30, P3), pixrgb( 1, 1, 100, 100, 100, 20, P4), image( 2, 2,[ P1, P2, P3, P4], I1).
 
 Crear una imagen tipo Hexmap.
-pixhex( 0, 0, "1312", 10, PA), 
-pixhex( 0, 1, "0321", 20, PB), 
-pixhex( 1, 0, "0312", 30, PC), 
-pixhex( 1, 1, "1312", 4, PD), 
-image( 2, 2, [PA, PB, PC, PD], I).
+pixhex( 0, 0, "#FFFFFF", 10, PA), pixhex( 0, 1, "#000000", 20, PB), pixhex( 1, 0, "#000000", 30, PC), pixhex( 1, 1, "#FFFFFF", 40, PD), image( 2, 2, [PA, PB, PC, PD], I).
+pixhex( 0, 0, "#00C7C7", 15, PA), pixhex( 0, 1, "#00C7C7", 15, PB), pixhex( 1, 0, "#00E9E9", 50, PC), pixhex( 1, 1, "#FFE9E9", 50, PD), image( 2, 2, [PA, PB, PC, PD], I).
+pixhex( 0, 0, "#D90000", 1, PA), pixhex( 0, 1, "#D80000", 2, PB), pixhex( 1, 0, "#E90000", 3, PC), pixhex( 1, 1, "#E90000", 4, PD), image( 2, 2, [PA, PB, PC, PD], I).
 
 Invertir horizontalmente una imagen tipo pixbit.
 pixbit( 0, 0, 1, 10, PA), 
